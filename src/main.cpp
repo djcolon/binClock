80736d00521b6558b969db9b56d0782a3b7a7ed0
dcolon <djcolon90@gmail.com>
2024-02-20 19:29:32 +0000

    Rewrote to have actual clock functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -7,44 +7,41 @@
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
 
 #define INTERNAL_LED D4
-#define MIN_CLOCK D0
-#define MIN_LATCH D1
-#define MIN_DATA D2
-#define HR_CLOCK D5
-#define HR_LATCH D6
-#define HR_DATA D7
+#define CLOCK D0
+#define LATCH D1
+#define DATA D2
 
-byte minRegister = 0;
-// Our leds are on bit 1-7
-byte minRegisterPos = 1;
-byte hrRegister = 0;
-byte hrRegisterPos = 1;
+union Registers {
+  uint8_t register1;
+  uint8_t register2;
+  uint8_t register3;
+  uint8_t register4;
+};
 
-void updateMinRegister() {
-  digitalWrite(MIN_LATCH, LOW);
-  shiftOut(MIN_DATA, MIN_CLOCK, MSBFIRST, minRegister);
-  digitalWrite(MIN_LATCH, HIGH);
+Registers registers;
+
+void updateRegisters() {
+  digitalWrite(LATCH, LOW);
+  shiftOut(DATA, CLOCK, MSBFIRST, registers.register1);
+  shiftOut(DATA, CLOCK, MSBFIRST, registers.register2);
+  shiftOut(DATA, CLOCK, MSBFIRST, registers.register3);
+  shiftOut(DATA, CLOCK, MSBFIRST, registers.register4);
+  digitalWrite(LATCH, HIGH);
 }
 
-void updateHrRegister() {
-  digitalWrite(HR_LATCH, LOW);
-  shiftOut(HR_DATA, HR_CLOCK, LSBFIRST, hrRegister);
-  digitalWrite(HR_LATCH, HIGH);
-}
+
 
 void setup() {
   // Pins
   pinMode(INTERNAL_LED, OUTPUT);
-  pinMode(MIN_CLOCK, OUTPUT);
-  pinMode(MIN_LATCH, OUTPUT);
-  pinMode(MIN_DATA, OUTPUT);
-  pinMode(HR_CLOCK, OUTPUT);
-  pinMode(HR_LATCH, OUTPUT);
-  pinMode(HR_DATA, OUTPUT);
+  pinMode(DATA, OUTPUT);
+  pinMode(CLOCK, OUTPUT);
+  pinMode(LATCH, OUTPUT);
   // Serial
   Serial.begin(115200);
   // WiFi
-  //Local intialization. Once its business is done, there is no need to keep it around
+  //Local initialization. Once its business is done, there is no need to keep
+  // it around.
   WiFiManager wifiManager;
   //reset saved settings
   //wifiManager.resetSettings();
@@ -59,24 +56,31 @@ void setup() {
 }
 
 void loop() {
-  // Minutes
-  if(minRegisterPos > 6) {
-    minRegisterPos = 1;
-    minRegister = 0;
-  } else {
-    bitSet(minRegister, minRegisterPos++);
-    digitalWrite(INTERNAL_LED, minRegisterPos % 2 == 0 ? HIGH : LOW);
+  // Update seconds.
+  registers.register1++;
+  if(registers.register1 == 60) {
+    registers.register1 = 0;
+    // Seconds rolled over, increase minutes.
+    registers.register2++;
   }
-  updateMinRegister();
 
-  // Hours
-  if(hrRegisterPos > 6) {
-    hrRegisterPos = 1;
-    hrRegister = 0;
-  } else {
-    bitSet(hrRegister, hrRegisterPos++);
+  // Update minutes.
+  registers.register2++;
+  // We may have had a double increase, this comparison and subtraction will
+  // deal with that.
+  if(registers.register2 >= 60) {
+    registers.register2 = registers.register2 - 60;
+    // Minutes rolled over, increase hours.
+    registers.register3++;
   }
-  updateHrRegister();
 
-  delay(200);
+  // Update hours.
+  registers.register3++;
+  if(registers.register3 > 24) {
+    registers.register3 = registers.register3 - 24;
+  }
+  
+  updateRegisters();
+
+  delay(1000);
 }
