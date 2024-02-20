c007b546124831eb9e26d10e257d9ba4eec5d2e8
dcolon <djcolon90@gmail.com>
2024-02-20 20:00:05 +0000

    Using built-in NTP to set time.

--- src/main.cpp
+++ src/main.cpp
@@ -6,30 +6,61 @@
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
 
+// Time
+// https://randomnerdtutorials.com/esp32-ntp-timezones-daylight-saving/
+#include "time.h"
+const String TZ = "GMT0BST,M3.5.0/1,M10.5.0;";
+
+// Pins
 #define INTERNAL_LED D4
 #define CLOCK D0
 #define LATCH D1
 #define DATA D2
 
-union Registers {
+struct RegistersStruct {
   uint8_t register1;
   uint8_t register2;
   uint8_t register3;
   uint8_t register4;
 };
 
-Registers registers;
+union RegistersUnion {
+  uint32_t asInt;
+  RegistersStruct asStruct;
+};
+
+RegistersUnion registers;
 
 void updateRegisters() {
   digitalWrite(LATCH, LOW);
-  shiftOut(DATA, CLOCK, MSBFIRST, registers.register1);
-  shiftOut(DATA, CLOCK, MSBFIRST, registers.register2);
-  shiftOut(DATA, CLOCK, MSBFIRST, registers.register3);
-  shiftOut(DATA, CLOCK, MSBFIRST, registers.register4);
+  shiftOut(DATA, CLOCK, MSBFIRST, registers.asStruct.register1);
+  shiftOut(DATA, CLOCK, MSBFIRST, registers.asStruct.register2);
+  shiftOut(DATA, CLOCK, MSBFIRST, registers.asStruct.register3);
+  shiftOut(DATA, CLOCK, MSBFIRST, registers.asStruct.register4);
   digitalWrite(LATCH, HIGH);
 }
 
+// Sets TZ from POSIX style TZ.
+// See: https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv
+void setTimezone(String timezone){
+  Serial.printf("Setting Timezone to %s\n",timezone.c_str());
+  setenv("TZ",timezone.c_str(),1);  //  Now adjust the TZ.  Clock settings are adjusted to show the new local time
+  tzset();
+}
 
+void initTime(String timezone){
+  struct tm timeinfo;
+
+  Serial.println("Setting up time");
+  configTime(0, 0, "pool.ntp.org");    // First connect to NTP server, with 0 TZ offset
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("  Failed to obtain time");
+    return;
+  }
+  Serial.println("  Got the time from NTP");
+  // Now we can set the real timezone
+  setTimezone(timezone);
+}
 
 void setup() {
   // Pins
@@ -53,33 +84,24 @@ void setup() {
   wifiManager.autoConnect("binClock");
   //if you get here you have connected to the WiFi
   Serial.println("Connected to WiFi. Starting.");
+  // Getting time.
+  initTime(TZ);
 }
 
 void loop() {
-  // Update seconds.
-  registers.register1++;
-  if(registers.register1 == 60) {
-    registers.register1 = 0;
-    // Seconds rolled over, increase minutes.
-    registers.register2++;
+  // Get the latest time.
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time 1");
+    // Turn on all LEDS to signal an error.
+    registers.asInt = UINT32_MAX;
   }
-
-  // Update minutes.
-  registers.register2++;
-  // We may have had a double increase, this comparison and subtraction will
-  // deal with that.
-  if(registers.register2 >= 60) {
-    registers.register2 = registers.register2 - 60;
-    // Minutes rolled over, increase hours.
-    registers.register3++;
-  }
-
-  // Update hours.
-  registers.register3++;
-  if(registers.register3 > 24) {
-    registers.register3 = registers.register3 - 24;
-  }
+  registers.asStruct.register1 = timeinfo.tm_hour;
+  registers.asStruct.register2 = timeinfo.tm_min;
+  registers.asStruct.register3 = timeinfo.tm_sec;
   
+  Serial.printf("%d:%d:%d\n", registers.asStruct.register1, registers.asStruct.register2, registers.asStruct.register3);
+
   updateRegisters();
 
   delay(1000);
