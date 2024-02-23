5dadd850502cfea9427364576b91c9a9cabf6b41
dcolon <djcolon90@gmail.com>
2024-02-23 08:17:50 +0000

    Some work on shfirout function, will need to get the scope out to find reason for unpredictable behaviour on some of the regsiters.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <Arduino.h>
 
 // WiFi
-#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include <ESP8266WiFi.h> //https://github.com/esp8266/Arduino
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
-#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+#include <WiFiManager.h> //https://github.com/tzapu/WiFiManager
 
 // Time
 // https://randomnerdtutorials.com/esp32-ntp-timezones-daylight-saving/
@@ -17,21 +17,59 @@ const String TZ = "GMT0BST,M3.5.0/1,M10.5.0;";
 #define LATCH D1
 #define DATA D2
 
-struct RegistersStruct {
+// Flags
+#define WRITEMSBFIRST
+#define DEBUG
+#define USEARDUINOSHIFT
+
+struct RegistersStruct
+{
   uint8_t register1;
   uint8_t register2;
   uint8_t register3;
   uint8_t register4;
 };
 
-union RegistersUnion {
+union RegistersUnion
+{
   uint32_t asInt;
   RegistersStruct asStruct;
 };
 
 RegistersUnion registers;
 
-void updateRegisters() {
+// Shifts out our register in one go.
+// Arduino shiftOut may because of weird glitches?
+// Data is set on rising edge of clock. Hold time is 4.5ns (datasheet)
+// This gives a rate of 222MHz, given that the chips is clocked at 26MHz, the
+// factor 10 means we won't have to worry about timing. Chip runs faster than
+// our program (especially with Arduino libs in between)!
+void shiftOut32()
+{
+  digitalWrite(LATCH, LOW);
+  bool bit;
+  for (uint8_t i = 0; i < 32; i++)
+  {
+    #ifdef WRITEMSBFIRST
+      bit = !!(registers.asInt & (1 << (32 - i)));
+    #else
+      bit = !!(registers.asInt & (1 << i));
+    #endif
+    digitalWrite(DATA, bit);
+    #ifdef DEBUG
+      Serial.print(bit);
+    #endif
+    digitalWrite(CLOCK, HIGH);
+    digitalWrite(CLOCK, LOW);
+  }
+  digitalWrite(LATCH, HIGH);
+  #ifdef DEBUG
+    Serial.print('\n');
+  #endif
+}
+
+void ArduinoShiftOut32()
+{
   digitalWrite(LATCH, LOW);
   digitalWrite(CLOCK, LOW);
   shiftOut(DATA, CLOCK, MSBFIRST, registers.asStruct.register1);
@@ -46,18 +84,21 @@ void updateRegisters() {
 
 // Sets TZ from POSIX style TZ.
 // See: https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv
-void setTimezone(String timezone){
-  Serial.printf("Setting Timezone to %s\n",timezone.c_str());
-  setenv("TZ",timezone.c_str(),1);  //  Now adjust the TZ.  Clock settings are adjusted to show the new local time
+void setTimezone(String timezone)
+{
+  Serial.printf("Setting Timezone to %s\n", timezone.c_str());
+  setenv("TZ", timezone.c_str(), 1); //  Now adjust the TZ.  Clock settings are adjusted to show the new local time
   tzset();
 }
 
-void initTime(String timezone){
+void initTime(String timezone)
+{
   struct tm timeinfo;
 
   Serial.println("Setting up time");
-  configTime(0, 0, "pool.ntp.org");    // First connect to NTP server, with 0 TZ offset
-  if(!getLocalTime(&timeinfo)){
+  configTime(0, 0, "pool.ntp.org"); // First connect to NTP server, with 0 TZ offset
+  if (!getLocalTime(&timeinfo))
+  {
     Serial.println("  Failed to obtain time");
     return;
   }
@@ -66,43 +107,47 @@ void initTime(String timezone){
   setTimezone(timezone);
 }
 
-void setup() {
+void setup()
+{
   // Pins
   pinMode(INTERNAL_LED, OUTPUT);
   pinMode(DATA, OUTPUT);
   pinMode(CLOCK, OUTPUT);
   pinMode(LATCH, OUTPUT);
+  digitalWrite(CLOCK, LOW);
   // Set leds to init.
   registers.asStruct.register2 = 0;
-  updateRegisters();
+  shiftOut32();
   // Serial
   Serial.begin(115200);
 
   registers.asStruct.register2 = 1;
-  updateRegisters();
+  shiftOut32();
   // WiFi
-  //Local initialization. Once its business is done, there is no need to keep
+  // Local initialization. Once its business is done, there is no need to keep
   // it around.
   WiFiManager wifiManager;
-  //reset saved settings
-  //wifiManager.resetSettings();
-  
-  //fetches ssid and pass from eeprom and tries to connect
-  //if it does not connect it starts an access point with the specified name
-  //here  "AutoConnectAP"
-  //and goes into a blocking loop awaiting configuration
+  // reset saved settings
+  // wifiManager.resetSettings();
+
+  // fetches ssid and pass from eeprom and tries to connect
+  // if it does not connect it starts an access point with the specified name
+  // here  "AutoConnectAP"
+  // and goes into a blocking loop awaiting configuration
   wifiManager.autoConnect("binClock");
-  //if you get here you have connected to the WiFi
+  // if you get here you have connected to the WiFi
   Serial.println("Connected to WiFi. Starting.");
   registers.asStruct.register2 = 4;
   // Getting time.
   initTime(TZ);
 }
 
-void loop() {
+void loop()
+{
   // Get the latest time.
   struct tm timeinfo;
-  if(!getLocalTime(&timeinfo)){
+  if (!getLocalTime(&timeinfo))
+  {
     Serial.println("Failed to obtain time 1");
     // Turn on all LEDS to signal an error.
     registers.asInt = UINT32_MAX;
@@ -110,10 +155,16 @@ void loop() {
   registers.asStruct.register1 = timeinfo.tm_hour;
   registers.asStruct.register2 = timeinfo.tm_min;
   registers.asStruct.register3 = timeinfo.tm_sec;
-  
-  Serial.printf("%02d:%02d:%02d\n", registers.asStruct.register1, registers.asStruct.register2, registers.asStruct.register3);
 
-  updateRegisters();
+  #ifdef DEBUG
+    Serial.printf("%02d:%02d:%02d\n", registers.asStruct.register1, registers.asStruct.register2, registers.asStruct.register3);
+  #endif
+
+  #ifdef USEARDUINOSHIFT
+    ArduinoShiftOut32();
+  #else
+    shiftOut32();
+  #endif
 
   delay(1000);
 }
